0c0e38921c46bee5e116140475452bc3b9d63635
HighlySupreme <gal.1999k@gmail.com>
2023-04-21 11:11:12 +0200

    Added delete, create and update rest Added simple delete create and update for tasks

--- src/services/rest.service.js
+++ src/services/rest.service.js
@@ -6,12 +6,25 @@ const getTasks = async () => {
     return axiosService.GET(`/api/${apiVersion}/task`)
         .then(r => r.data)
 }
+
+const createTask = async (task) => {
+    return axiosService.POST(`/api/${apiVersion}/task`, task)
+        .then(r => r.data)
+}
+
+const updateTask = async (task) => {
+    return axiosService.PUT(`/api/${apiVersion}/task`, task)
+        .then(r => r.data)
+}
+
 const deleteTasks = async (tasks) => {
-    return axiosService.DELETE(`/api/${apiVersion}/task/delete`, tasks)
+    return axiosService.DELETE(`/api/${apiVersion}/task`, tasks)
         .then(r => r.data)
 }
 
 export default {
     getTasks,
+    createTask,
+    updateTask,
     deleteTasks
 }
