6c8244e2316ac4d130af3b22c8825742ca4063f2
HighlySupreme <gal.1999k@gmail.com>
2023-04-21 09:23:20 +0200

    Added REST for delete tasks Added base REST for delete tasks

--- src/services/rest.service.js
+++ src/services/rest.service.js
@@ -3,10 +3,15 @@ import axiosService from "src/services/axios.service";
 const apiVersion = "v1"
 
 const getTasks = async () => {
-    return axiosService.GET(`/api/${apiVersion}/tasks`)
+    return axiosService.GET(`/api/${apiVersion}/task`)
+        .then(r => r.data)
+}
+const deleteTasks = async (tasks) => {
+    return axiosService.DELETE(`/api/${apiVersion}/task/delete`, tasks)
         .then(r => r.data)
 }
 
 export default {
-    getTasks
+    getTasks,
+    deleteTasks
 }
